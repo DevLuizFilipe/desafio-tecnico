0fda3228f8ba32acebcffc5bf6405cfa57e6c55d
Luiz Filipe Santana Martins <lfelips6@gmail.com>
2024-03-25 13:08:02 -0300

    fix: fixing bucket creation

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,8 +1,3 @@
-resource "aws_s3_bucket" "bucket-front-end" {
-  bucket = "globo-desafio-tecnico-devops"
-  acl    = "public-read"
-}
-
 resource "aws_s3_bucket_object" "object" {
   bucket = "globo-desafio-tecnico-devops"
   key    = "index.html"
