001837f71268f4e2e6db1b54b35750574e1412d3
Luiz Filipe Santana Martins <lfelips6@gmail.com>
2024-03-25 12:46:18 -0300

    fix: fixing bucket s3

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,21 +1,11 @@
-# module "bucket-front-end" {
-#   source                  = "terraform-aws-modules/s3-bucket/aws"
-#   version                 = "3.13.0"
-#   bucket                  = "globo-desafio-tecnico-devops"
-#   block_public_acls       = false
-#   block_public_policy     = false
-#   ignore_public_acls      = false
-#   restrict_public_buckets = false
-#   acl                     = "public-read"
+resource "'aws_s3_bucket" "bucket-front-end" {
+  bucket = "globo-desafio-tecnico-devops"
+  acl    = "public-read"
+}
 
-#   website = {
-#     index_document = "index.html"
-#   }
-# }
-
-# resource "aws_s3_bucket_object" "object" {
-#   bucket = module.bucket-front-end.s3_bucket_id
-#   key    = "index.html"
-#   source = "../front/index.html"
-#   acl    = "public-read"
-# }
+resource "aws_s3_bucket_object" "object" {
+  bucket = module.bucket-front-end.s3_bucket_id
+  key    = "index.html"
+  source = "../front/index.html"
+  acl    = "public-read"
+}
