bc0bf5c62cf28f2ea83de731b911fa0ad5501073
Luiz Filipe Santana Martins <lfelips6@gmail.com>
2025-11-13 23:06:11 -0300

    fix: corrigir referência ao ARN do bucket S3 na política de acesso

--- infra/s3.tf
+++ infra/s3.tf
@@ -26,7 +26,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
         Effect    = "Allow"
         Principal = "*"
         Action    = ["s3:GetObject"]
-        Resource  = "${aws_s3_bucket.frontend.arn}/*"
+        Resource  = "${aws_s3_bucket.bucket.arn}/*"
       }
     ]
   })
